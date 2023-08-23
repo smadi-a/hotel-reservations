a6a08e44f05c10eb516971c856e85c9cfd34a672
Ali Smadi <alielsmadi@hotmail.com>
2023-08-24 09:16:25 +1000

    build reservation schema parser

---
+++ app/services/reservations/schema.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Reservations
+  class Schema
+    def initialize(schema_hash)
+      @schema_hash = schema_hash
+    end
+
+    def parse_data(params)
+      @params = params
+      return unless schema_hash
+
+      reservation_hash.merge(guest_params: guest_hash)
+    end
+
+    private
+
+    attr_reader :schema_hash, :params
+
+    def reservation_hash
+      %i[code start_date end_date adults children infants status host_currency payout_price security_price].each_with_object({}) do |attribute, hash|
+        if schema_hash[attribute]
+          dig_params = schema_hash[attribute].split('#')
+          hash[attribute] = params.dig(*dig_params)
+        end
+        hash
+      end
+    end
+
+    def guest_hash
+      %i[email first_name last_name phone_numbers].each_with_object({}) do |attribute, hash|
+        if schema_hash["guest_#{attribute}".to_sym]
+          dig_params = schema_hash["guest_#{attribute}".to_sym].split('#')
+          hash[attribute] = params.dig(*dig_params)
+        end
+        hash
+      end
+    end
+  end
+end
