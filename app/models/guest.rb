afe8a3f77fd24e0a360bb413cab193dd7fcd6df9
Ali Smadi <alielsmadi@hotmail.com>
2023-08-25 07:13:56 +1000

    validate email address

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
